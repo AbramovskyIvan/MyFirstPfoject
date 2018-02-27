7e59104a427c98705bcc0a2ebd15c831b05fdb36
AbramovskyIvan <abramovsky.ivan@gmail.com>
2018-02-27 20:20:47 +0200

    pyramide_v2

--- 111.js
+++ 111.js
@@ -1,10 +1,11 @@
 var a = 4;
+//just give me the 'a' value and you'll see the magic
 var i = 1;
 var str = '';
 
 while (i <= a) {
-	for (var x = 1; x<=i; x++) {
-		str = ''+'#';
+	for (var x = 1; x <= i; x++) {
+		str = str + '#';
 	}
 	console.log(str);
 	str = '';
