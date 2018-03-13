d2f62b40217b07441c2c382d89abe15c9349f85f
AbramovskyIvan <abramovsky.ivan@gmail.com>
2018-03-13 20:33:20 +0200

    probe1

--- HashCode2018/HashCode2018.js
+++ HashCode2018/HashCode2018.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
-var fname = process.argv[2];
+//var fname = process.argv[2];
+var fname = './e_high_bonus.in'
 
 var array = fs.readFileSync(fname).toString().split("\n");
 var roads = [];
@@ -23,58 +24,94 @@ for (var i = 0; i <= vehicles - 1;  i++) {
 }
 
 for (var i = 1; i < array.length - 1; i++) {
-  roads[i-1] = {
-    start_x: array[i].split(" ")[0],
-    start_y: array[i].split(" ")[1],
-    finish_x: array[i].split(" ")[2],
-    finish_y: array[i].split(" ")[3],
-    start: array[i].split(" ")[4],
-    end: array[i].split(" ")[5],
-    len: Math.abs(array[i].split(" ")[0] - array[i].split(" ")[2]) + Math.abs(array[i].split(" ")[1] - array[i].split(" ")[3]),
+  arrayElement = array[i].split(" ");
+  roads[i - 1] = {
+    start_x: arrayElement[0],
+    start_y: arrayElement[1],
+    finish_x: arrayElement[2],
+    finish_y: arrayElement[3],
+    start: arrayElement[4],
+    end: arrayElement[5],
+    len: Math.abs(arrayElement[0] - arrayElement[2]) + Math.abs(arrayElement[1] - arrayElement[3]),
     finished: false
   };
 }
 
-var orderCalculation = (car, step) => {
-  console.log('INNNNN')
-  var newOrders = roads.map((order) => {
-    if (order.finished) {
-      return -9999999999;
-    }
-    var carCoords = car.order_id == null ? [0,0] : [roads[car.order_id].finish_x, roads[car.order_id].finish_y];
-    var dist = Math.abs(order.start_x - carCoords[0]) + Math.abs(order.start_y - carCoords[1]);
-    var temp_points = step + dist <= order.start ? 2 : 0;
-    var points = temp_points + order.len;
-    var wait = order.start - (step + dist) < 0 ? 0 : order.start - step + dist;
-    var endTime = step + dist + wait + order.len;
-    points = endTime > order.end ? 0 : points;
-    var indicator = points - wait - dist;
-    // console.log(result);
-    return indicator;
-  });
-  var best_order_id = newOrders.indexOf(Math.max(...newOrders));
+function orderCalculation (car, step, ord_ret) {
+  var newOrders = [];
 
-  console.log(best_order_id);
-  return best_order_id;
+  //console.log('INNNNN')
+  for (var i = 0; i < roads.length; i++) {
+    order = roads[i];
+    //console.log(order);
+    if (order.finished) {
+      newOrders[i] = null;
+    } else {
+      var carCoords = car.order_id == null ? [0,0] : [roads[car.order_id].finish_x, roads[car.order_id].finish_y];
+      var dist = Math.abs(order.start_x - carCoords[0]) + Math.abs(order.start_y - carCoords[1]);
+      var temp_points = step + dist <= order.start ? bonus : 0;
+      var points = temp_points + order.len;
+      var wait = order.start - (step + dist) < 0 ? 0 : order.start - step + dist;
+      var endTime = step + dist + wait + order.len;
+      points = endTime > order.end ? 0 : points;
+      var indicator = points - wait - dist;
+      newOrders[i] = indicator;
+    }
+  }
+  console.log(newOrders);
+  //var best_order_id = newOrders.indexOf(Math.max(newOrders));
+  //console.log(Math.max.apply(null, newOrders));
+  var best_order_id = newOrders.indexOf(Math.max.apply(null, newOrders));
+
+  //console.log(best_order_id);
+  if (ord_ret) {
+    return best_order_id;
+  } else {
+    return endTime;
+  }
+  
 }
 
 for (var step = 0; step <= steps; step++) {
   for (var i = 0; i <= cars.length - 1; i++) {
+    // console.log(step);
+    // console.log(cars[i].free_time);
+    // console.log("-----------------");
     if (cars[i].free_time == step) {
-      var bestId = orderCalculation(cars[i], step);
+      var bestId = orderCalculation(cars[i], step, true);
+      var cur_free_time = orderCalculation(cars[i], step, false);
+      // console.log("-----------------");
+      // console.log(bestId);
+      // console.log("-----------------");
+      cars[i].free_time = cur_free_time;
       cars[i].order_id = bestId;
-      cars[i].orders.push(bestId);
-      roads[bestId].finished = true;
+      try {
+        roads[bestId].finished = true;
+        cars[i].orders.push(bestId);
+      } catch(err) {
+
+      }
     };
   }
 }
 
-var fname = 'out.out';
-var ress = cars.map((i)=>i);
-cars.join("\n");
+var fname = './out.out';
+// var ress[];
+// for (var i = 0; i < cars.length; i++) {
+//   ress[i] = cars[i].
+// }
+// var ress = cars.map((i)->i);
+
+var resultText = "";
+for (var i = 0; i < cars.length; i++) {
+  resultText = resultText + i + " ";
+  resultText = resultText + cars[i].orders.join(" ");
+  resultText = resultText + "\n";
+}
+
 fs.open(fname, "w+", 0644, function(err, file_handle) {
 	if (!err) {
-	    fs.write(file_handle, ress, null, 'ascii', function(err, written) {
+	    fs.write(file_handle, resultText, null, 'ascii', function(err, written) {
 	        if (!err) {
 	            console.log("Text write succesfully");
 	        } else {
